6d445b2435681117842674975ced3b21893f7d68
Oleksandr Tovkailo <tov4ik@gmail.com>
2025-07-19 15:46:25 +0700

    Task #4 done

--- main.py
+++ main.py
@@ -27,23 +27,109 @@
 
 # Завдання 2
 
-def generator_numbers(text: str):
-    # Розбиваємо текст на слова за пробілами
-    for word in text.split():
+# def generator_numbers(text: str):
+#     # Розбиваємо текст на слова за пробілами
+#     for word in text.split():
+#         try:
+#             # Пробуємо перетворити слово на float
+#             # Якщо вдається — це дійсне число доходу, повертаємо його
+#             yield float(word)
+#         except ValueError:
+#             # Ігноруємо якщо перетворення не вдалося 
+#             continue
+
+# def sum_profit(text: str, func):
+#     # Викликаємо генератор і підсумовуємо всі знайдені числа
+#     return sum(func(text))
+
+
+
+# text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+# total_income = sum_profit(text, generator_numbers)
+# print(f"Загальний дохід: {total_income}")
+
+# ***************************************************************
+
+# Завдання 4
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
         try:
-            # Пробуємо перетворити слово на float
-            # Якщо вдається — це дійсне число доходу, повертаємо його
-            yield float(word)
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found."
         except ValueError:
-            # Ігноруємо якщо перетворення не вдалося 
-            continue
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter user name."
+    return wrapper
 
-def sum_profit(text: str, func):
-    # Викликаємо генератор і підсумовуємо всі знайдені числа
-    return sum(func(text))
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
 
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
 
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        raise KeyError
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+@input_error
+def show_all(contacts):
+    if not contacts:
+        return "No contacts saved."
+    result = []
+    for name, phone in contacts.items():
+        result.append(f"{name}: {phone}")
+    return "\n".join(result)
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    
+    while True:
+        user_input = input("Enter a command: ")
+        if not user_input.strip():
+            continue  # ignore empty lines
+
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, contacts))
+
+        elif command == "change":
+            print(change_contact(args, contacts))
+
+        elif command == "phone":
+            print(show_phone(args, contacts))
+
+        elif command == "all":
+            print(show_all(contacts))
+
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
